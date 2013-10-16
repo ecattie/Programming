03e17d0f3fc1402dda47f7e905870cfb3e8b57e7
OpenShift guest <ebjc@aol.com>
2013-10-16 16:52:48 -0400

    now

---
+++ scripts.js
@@ -0,0 +1,38 @@
+var todo = ["Dust","Vacuum","Dishes","Laundry"];
+document.write('<li>' + todo + '</li>');
+
+for (var i = 0; i < todo.length; i++)
+var todo = todo[i];
+
+
+
+
+
+
+/*
+// This looks much nicer a separate call for each array member
+var books = [
+   "Fifty Shades of Grey",
+   "Twilight",
+   "The Notebook"
+];
+
+var html = "";
+for (var i =0; i < books.length; i++) {
+    html += "<li>" + books[i]+ "</li>";
+}
+document.getElementById("books").innerHTML = html;
+*/
+
+/*
+var i;
+var mycars = new Array();
+mycars[0] = "Saab";
+mycars[1] = "Volvo";
+mycars[2] = "BMW";
+
+for (i=0;i<mycars.length;i++)
+{
+document.write(mycars[i] + "<br>");
+}
+*/
