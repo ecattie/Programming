df0a4ff2bfdcc89092ffa0b8ff945058f1ac74a8
OpenShift guest <ebjc@aol.com>
2013-10-25 22:04:36 -0400

    hmwk2complete

--- scripts.js
+++ scripts.js
@@ -1,74 +1,48 @@
 
 //String Object Variable
-
+/*
 var toDo = ["Dust","Vacuum","Dishes","Laundry"];
-    for (var i = 0; i < toDo.length; i++){
-document.write('<li>' + toDo[i] + '</li>');
+   document.write("<ul class='list-group'>");
+   for (var i = 0; i < toDo.length; i++){
+document.write("<li class= 'list-group-item'>" + toDo[i] + '</li>');
 }
+document.write("</ul>");
+*/
+
 
 
 //Array
 
-var toDo = {};
-toDo[1] = {name: "Dust", due:"Tuesday", complete:true};
-toDo[2] = {name:"Vacuum", due:"Wednesday", complete:true};
-toDo[3] = {name:"Dishes", due:"Thursday", complete:true};
-toDo[4] = {name:"Laundry", due:"Friday", complete:true};
+var toDo = [{name:"Dust the house.", due:"Tuesday", 'complete':true},
+{name:"Vacuum upstsair and downstairs.", due:"Wednesday", 'complete':true},
+{name:"Wash the dishes.", due:"Thursday", 'complete':true},
+{name:'Do the laundry.', due:"Friday", 'complete':true}];
 
-for (var i = 0; i <= 3; i++){
-  document.write("<li>" + name + "</li>");
-}
+  document.write("<ul class='list-group'>");
 
-/*
-function lastElement(array) {
-  if (array.length > 0)
-    return array[array.length - 1];
-  else
-    return undefined;
-}
+for (var i = 0; i < toDo.length; i++){
+    
 
-show(lastElement([1, 2, undefined]));
-*/
-/*
-//correct example
-var myObject = {};
-myObject["123"] = { "A": 123, "B": 456 };  // add 
-myObject["123"] = { "C": 123, "D": 456 };  // add more elements to array in element!
-myObject["124"] = { "A": 123, "B": 456 };
-myObject["125"] = { "A": 123, "B": 456 };
-console.log(myObject);
-
-// 2. delete first property in array
-for (property in myObject){
-        delete myObject[property];
-        break;
+  document.write('<li class= "list-group-item">' + toDo[i].name + '</li>');
+  
+  try{
+    compList(toDo[i]);
+    }
+    catch(error){
+        alert("There was an error:" + error);
+    } 
+  
     }
 
-// 3. show first remaining property
-for (property in myObject){
-        x = property;
-        console.log("first remaining property is " + x );
-        break;
+document.write('</ul>');
+
+
+function compList(list){
+     if (typeof list.name == "null") {
+    throw 'WARNING!!!';
     }
-*/
-
-
-/*
-function trainFare(zone, roundTrip, offPeakHours) {
-
-
-// get base fare from value of zone - 1 through 6
-        var baseFare = zone;
-        
-// calculate discount if roundTrip is true (double baseFare, subtract $0.50);
-        if (roundTrip == true){
-            baseFare = (baseFare * 2 - .50)
-        } 
-// calculate another discount is offPeakHours is true (another $0.50 off.)
-         if (offPeakHours == true){
-            baseFare = (baseFare - .50)
-         }
-// return fare
-    return baseFare;
+   
 }
-*/
+
+
+
