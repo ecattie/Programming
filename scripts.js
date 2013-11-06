d4c60de3313ad9ac22d9c0331b689ab70e744426
OpenShift guest <ebjc@aol.com>
2013-11-06 00:09:37 -0500

    hmwk3

--- scripts.js
+++ scripts.js
@@ -10,19 +10,18 @@ document.write("</ul>");
 */
 
 
-
+/*
 //Array
 
-var toDo = [{name:"Dust the house.", due:"Tuesday", 'complete':true},
-{name:"Vacuum upstsair and downstairs.", due:"Wednesday", 'complete':true},
-{name:"Wash the dishes.", due:"Thursday", 'complete':true},
-{name:'Do the laundry.', due:"Friday", 'complete':true}];
+var toDo = [{'name':"Dust the house.", 'due':"Tuesday", 'complete':true},
+{'name':"Vacuum upstsair and downstairs.", 'due':"Wednesday", 'complete':true},
+{'name':"Wash the dishes.", 'due':"Thursday", 'complete':true},
+{'name':'Do the laundry.', 'due':"Friday", 'complete':true}];
 
   document.write("<ul class='list-group'>");
 
 for (var i = 0; i < toDo.length; i++){
     
-
   document.write('<li class= "list-group-item">' + toDo[i].name + '</li>');
   
   try{
@@ -43,6 +42,53 @@ function compList(list){
     }
    
 }
+*/
+
+
+//jquery CRUD
+
+function printRule(rule){
+    if (typeof rule.complete == 'undefined'){
+        throw('"The complete status is missing for" + toDo[i].name + "."');
+    }
+
+    if (rule.complete === true) {
+        return ('<li class = "list-group-item">' + 'Done' + '</li>');
+    
+    } else { 
+        return ('<li class = "list-group-item">' + rule.name + rule.due + '</li>');
+        }
+}
+
+$(document).ready(function() { 
+    var toDo = 
+        [{name:'Dust the house.', due:'Tuesday', 'complete':true},
+        {name:'Vacuum upstair and downstairs.', due:'Wednesday', 'complete':false},
+        {name:'Wash the dishes.', due:'Thursday', 'complete':true},
+        {name:'Do the laundry.', due:'Friday', 'complete':false}];
+
+var chores = '<ul class="list-group">';
+
+    for (var i = 0; i < toDo.length; i++) {
+//        $('<li class= "list-group-item">').append(toDo[i] + '</li>');
+
+  
+      try{
+        chores += printRule(toDo[i]);
+        }
+        catch(error) {
+            console.log("There was an error:" + error);
+        } 
+  
+    }
+
+chores += '</ul>';
+
+console.log(chores);
+
+$('.container').append(chores);
+
+});
 
 
 
